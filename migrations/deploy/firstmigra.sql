93d7ceb463e8306bb8847c2616df443e9ab81ea0
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-12-06 16:54:20 +0100

    insert fake data in cat table

--- migrations/deploy/firstmigra.sql
+++ migrations/deploy/firstmigra.sql
@@ -18,6 +18,13 @@ CREATE TABLE "vote" (
   "updated_at" TIMESTAMP
 );
 
+INSERT INTO "cat" ("name", "image_path") VALUES
+('MTgwODA3MA', 'http://24.media.tumblr.com/tumblr_m82woaL5AD1rro1o5o1_1280.jpg'),
+('tt', 'http://24.media.tumblr.com/tumblr_m29a9d62C81r2rj8po1_500.jpg'),
+('bmp', 'http://25.media.tumblr.com/tumblr_m4bgd9OXmw1qioo2oo1_500.jpg'),
+('c8a', 'http://24.media.tumblr.com/tumblr_lzxok2e2kX1qgjltdo1_1280.jpg');
+
+
 
 
 
